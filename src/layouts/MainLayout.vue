3d5c09fa39720b03bba186923cc7b2a46bdc5085
fueremi <rianoktiomersaputra4@gmail.com>
2021-05-27 09:18:53 +0700

    Initiate Todo Apps; Create todo and help components

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,101 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Quasar - Todo Apps</div>
+        <div class="text-subtitle1">{{ todaysDate }}</div>
+      </div>
+      <q-img src="../assets/mountain.jpg" class="header-image absolute-top" />
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="300"
+      :breakpoint="700"
+    >
+      <q-scroll-area
+        style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd"
+      >
+        <q-list padding>
+          <q-item to="/" clickable v-ripple exact>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section>
+              Todo
+            </q-item-section>
+          </q-item>
+          <q-item to="/help" clickable v-ripple exact>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section>
+              Help
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top"
+        src="../assets/mountain.jpg"
+        style="height: 192px"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+          <div class="text-weight-bold">Rian Oktio Mersa Putra</div>
+          <div>@fueremi</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from "quasar";
+
+export default {
+  name: "MainLayout",
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  },
+  computed: {
+    todaysDate() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd, D MMMM YYYY");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis maxime totam eos sed dolore quidem amet tempora voluptate voluptatibus fugiat.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, ipsam, tempore quam ad dicta error pariatur id praesentium corporis animi ipsum maxime eum rem nemo.</p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
