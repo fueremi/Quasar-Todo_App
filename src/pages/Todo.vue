7bcaeb8aa6f57d2a407ae240f52033cbdc659965
fueremi <rianoktiomersaputra4@gmail.com>
2021-05-27 11:22:59 +0700

    finishing todo app

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,5 +1,22 @@
 <template>
   <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg-primary">
+      <q-input
+        @keyup.enter="addTask"
+        class="col"
+        square
+        filled
+        bg-color="white"
+        v-model="newTask"
+        placeholder="Add task"
+        dense
+      >
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" />
+        </template>
+      </q-input>
+    </div>
+
     <q-list class="bg-white" separator bordered>
       <q-item
         v-for="(task, index) in tasks"
@@ -31,6 +48,12 @@
         </q-item-section>
       </q-item>
     </q-list>
+    <div v-if="!tasks.length" class="no-tasks absolute-center">
+      <q-icon name="check" size="100px" color="primary" />
+      <div class="text-h5 text-primary text-center">
+        No Tasks
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -38,20 +61,8 @@
 export default {
   data() {
     return {
-      tasks: [
-        {
-          title: "Get Bananas",
-          done: false
-        },
-        {
-          title: "Eat Bananas",
-          done: false
-        },
-        {
-          title: "Poo Bananas",
-          done: false
-        }
-      ]
+      newTask: "",
+      tasks: []
     };
   },
   methods: {
@@ -66,8 +77,17 @@ export default {
         .onOk(() => {
           // console.log('>>>> OK')
           this.tasks.splice(index, 1);
-          this.$q.notify('Task deleted!')
+          this.$q.notify("Task deleted!");
         });
+    },
+    addTask() {
+      this.tasks.push({
+        title: this.newTask,
+        done: false
+      });
+
+      this.$q.notify("Task added!");
+      this.newTask = "";
     }
   }
 };
@@ -79,5 +99,9 @@ export default {
     text-decoration: line-through;
     color: #bbb;
   }
+}
+
+.no-tasks {
+  opacity: 0.5;
 }
 </style>
